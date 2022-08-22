d618437268173a8f39631baef9988efb39aad4ec
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-08-22 14:38:57 +0200

    delete recettes KO

--- recipes/delete.php
+++ recipes/delete.php
@@ -8,20 +8,31 @@ $getData = $_GET;
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de Recettes - Supprimer la recette ?</title>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-        rel="stylesheet"
-    >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
-<?php include_once($rootPath.'/header.php'); ?>
-<h1>Supprimer la recette ?</h1>
-<form action="<?php echo($rootUrl . 'recipes/post_delete') ?>"></form>
-    
+        <?php include_once('../header.php'); ?>
+        <h1>Supprimer la recette ?</h1>
+        <form action="<?php echo ($rootUrl . 'recipes/post_delete') ?>" method="POST">
+            <div class="mb-3 visually-hidden">
+
+                <label for="id" class="form-label">Identifiant de la recette</label>
+                <input type="hidden" class="form-control" id="id" name="id" value="<?php echo ($getData['id']); ?>">
+            </div>
+
+            <button type="submit" class="btn btn-danger">la supression est définitive </button>
+        </form>
+        <br />
     </div>
+    <?php include_once('../footer.php'); ?>
+</body>
+
+</html>
